a17c27de767b59351695ccd95308726a6eef0b8d
Muralidaran Vijayaraghavan <vmurali@csail.mit.edu>
2012-09-07 04:13:24 -0400

    stuff

--- CacheCoherence.v
+++ CacheCoherence.v
@@ -43,6 +43,41 @@ Section classic.
     Hypothesis fifo:
       forall s m te td, deq s m td -> enq s m te -> forall m' te' td', deq s m' td' -> enq s m' te' -> ((td' < td -> te' < te) /\ (te' < te -> td' < td)).
 
+    Theorem fifo':
+      forall {s m te td}, deq s m td -> enq s m te -> forall {m' te' td'}, deq s m' td' -> enq s m' te' -> ((td' <= td -> te' <= te) /\ (te' <= te -> td' <= td)).
+      Proof.
+        intros.
+        pose proof (fifo s m te td H H0 m' te' td' H1 H2).
+        destruct H3.
+        constructor.
+        intros.
+        assert (td' < td \/ td' >= td) by crush.
+        destruct H6.
+        specialize (H3 H6).
+        crush.
+        assert (td = td') by crush.
+        pose proof (uniqueDeq s m m' td td' H H1).
+        destruct H8.
+        specialize (H9 H7).
+        pose proof (uniqueEnq s m m' te te' H0 H2).
+        destruct H10.
+        specialize (H10 H9).
+        crush.
+        intros.
+        assert (te' < te \/ te' >= te) by crush.
+        destruct H6.
+        specialize (H4 H6).
+        crush.
+        assert (te = te') by crush.
+        pose proof (uniqueEnq s m m' te te' H0 H2).
+        destruct H8.
+        specialize (H9 H7).
+        pose proof (uniqueDeq s m m' td td' H H1).
+        destruct H10.
+        specialize (H10 H9).
+        crush.
+      Qed.
+
     Theorem send0Fifo:
       forall {s m t}, enq s m 0 -> deq s m t -> forall t', t' < t -> forall {m'}, ~ deq s m' t'.
     Proof.
@@ -275,12 +310,14 @@ Section classic.
         (forall m t1 t2, recvm p m t1 -> sendm c m t2 -> t1 <= tp -> t2 <= tc) ->
         (forall m t1 t2, recvm c m t1 -> sendm p m t2 -> t1 <= tc -> t2 <= tp) ->
         forall {tr}, tr <= tc -> forall {m}, (recvm c m tr /\ fst m <= state c tr) ->
-          (forall t, S tr <= t <= tc -> forall m', ~ (recvm c m' t /\ fst m' <= state c t)) ->
+          (forall t, S tr <= t <= tc -> ~ exists m', recvm c m' t /\ fst m' <= state c t) ->
           forall {ts}, sendm p m ts -> (forall t', S ts <= t' <= tp -> forall m'', ~ recvm p m'' t') ->
         ~ (nextState c tc > nextState p tp).
         unfold not.
         intros.
-        assert (nextState c tc <= nextState c tr) by (apply (noRecvChild2 H1 H3)).
+        assert (forall t, S tr <= t <= tc -> forall m', ~ (recvm c m' t /\ fst m' <= state c t)) by (generalize H3; clear; firstorder).
+        clear H3.
+        assert (nextState c tc <= nextState c tr) by (apply (noRecvChild2 H1 H7)).
         assert (ts <= tp) by (apply (H0 m tr); crush).
         assert (nextState p tp >= nextState p ts) by (apply (noRecvParent2 H8 H5)).
         assert (nextState p ts = snd m) by (apply sendCommon; crush).
@@ -333,10 +370,7 @@ Section classic.
         clear cRecv.
         destruct cRecvMax as [tcmax rest].
         destruct rest as [tcmaxLeTcmin rest].
-        destruct rest as [cRecvMax noCRecv'].
-        assert (noCRecv: forall y, S tcmax <= y <= tcmin -> forall m, ~ (recvm c m y /\ fst m <= state c y)) by
-          (generalize noCRecv'; clear; firstorder).
-        clear noCRecv'.
+        destruct rest as [cRecvMax noCRecv].
         destruct cRecvMax as [m childRecvCond].
         assert (exTpmax: exists tpmax, tpmax <= tcmax /\ sendm p m tpmax) by (apply deqImpEnq; crush).
         destruct exTpmax as [tpmax rest].
@@ -344,7 +378,6 @@ Section classic.
         clear useless.
         assert (pNotRecvGtTpmax: forall t, S tpmax <= t <= tpmin -> forall m, ~ recvm p m t) by
           (intros; assert (0 <= t <= tpmin) by crush; apply pNotRecv; crush).
-        pose proof (@childRecvNoParentRecv).
         apply (childRecvNoParentRecv pRecvCSend cRecvPSend tcmaxLeTcmin childRecvCond noCRecv sendPMTpmax pNotRecvGtTpmax wrongState).
         assert (pRecv'': exists t, 0 <= t <= tpmin /\ exists m, recvm p m t) by (generalize (dec (exists t, 0 <= t <= tpmin /\ exists m, recvm p m t)) pRecv'; clear; firstorder).
         clear pRecv'.
@@ -376,13 +409,15 @@ Section classic.
         clear nsPTpminGeNsPTp1 sCTc1EqFstM nsCTc1EqSndM nsCTcminGTNsPTp1 nsPTp1EqSndM nsCTc1LtNsCTcmin.
 
         pose proof (minExists dec exCRecv) as exCRecvMin.
-        assert (exCRecvUseful: exists t, t <= tcmin /\ exists m, recvm c m t /\ fst m <= state c t) by
-          (destruct exCRecv as [t hyp]; assert (t <= tcmin) by crush; generalize hyp; clear; firstorder).
+
+(*        assert (exCRecvUseful: exists t, t <= tcmin /\ exists m, recvm c m t /\ fst m <= state c t) by
+          (destruct exCRecv as [t hyp]; assert (t <= tcmin) by crush; generalize hyp; clear; firstorder). *)
         clear exCRecv.
         destruct exCRecvMin as [tc2 rest].
         destruct rest as [rest noCRecvGTTc1'].
         destruct rest as [tc1LtTc2LeTcmin exCRecv].
-        destruct exCRecv as [n rest].
+        generalize exCRecv; intro rest.
+        destruct rest as [n rest].
         destruct rest as [cRecvN fstN].
         assert (exPSend: exists tp2, tp2 <= tc2 /\ sendm p n tp2) by (apply deqImpEnq; crush).
         destruct exPSend as [tp2 rest].
@@ -404,9 +439,39 @@ Section classic.
         clear noCRecvGTTc1'.
         clear sCTc1GtNsCTc1 fstPN sndPN sPTp2LeScTc2 nsCtc2LeNsCTc1.
 
-        assert (tp1LeTp2OrNot: tp2 < tp1 \/ tp1 <= tp2) by (clear; crush).
-        destruct tp1LeTp2OrNot as [tp2LtTp1 | tp2GeTp1].
-        Focus 2.
+        assert (tp1LeTp2OrNot: tp1 <= tp2 \/ tp2 < tp1) by (clear; crush).
+        destruct tp1LeTp2OrNot as [tp1LeTp2 | tp2LtTp1].
+        assert (tc2LtTcmin: S tc2 <= tcmin) by crush.
+        pose proof (@maxExistsPower dec (fun t => exists m, recvm c m t /\ fst m <= state c t) tcmin (S tc2) tc2LtTcmin exCRecv) as exCRecvMax.
+        (* clear exCRecvUseful. *)
+        destruct exCRecvMax as [tc3 rest].
+        destruct rest as [tc2LtTc3LeTcmin rest].
+        destruct rest as [exCRecvTc3 noCRecvGtTc3].
+        destruct exCRecvTc3 as [n' cRecvTc3Fst].
+        generalize cRecvTc3Fst; intro hyp.
+        destruct hyp as [cRecvTc3 fstTc3].
+        assert (tc2LtTc3: S tc2 <= tc3) by crush.
+        assert (exPRecvTp3: exists tp3, tp3 <= tc3 /\ sendm p n' tp3) by (apply deqImpEnq; crush).
+        destruct exPRecvTp3 as [tp3 rest].
+        destruct rest as [junk pSendX].
+        clear junk.
+        pose proof (fifo' cRecvTc3 pSendX cRecvN pSendN) as conj.
+        destruct conj as [impTp2LeTp3 junk].
+        clear junk.
+        specialize (impTp2LeTp3 tc2LtTc3).
+        assert (noRecvPLate: forall y, S tp3 <= y <= tpmin -> forall m, ~ recvm p m y) by
+          (intros; assert (hyp: S tp1 <= y <= tpmin) by crush; generalize y hyp noRecvGTTp1; clear; firstorder).
+        assert (tc3LeTcmin: tc3 <= tcmin) by crush.
+        pose proof (childRecvNoParentRecv pRecvCSend cRecvPSend tc3LeTcmin cRecvTc3Fst noCRecvGtTc3 pSendX noRecvPLate).
+        crush.
+
+        destruct tp2.
+        pose proof @zeroRecvChild.
+        assert (tc1LeSTc2: tc1 <= S tc2) by crush.
+        pose proof (zeroRecvChild pSendN cRecvN tc1 tc1LeSTc2) as contra1.
+        clear tc1LeSTc2.
+        crush.
+
         assert (noRecvGtTp2: forall y, S tp2 <= y <= tpmin -> forall m, ~ recvm p m y) by
           (intros; assert (hyp: S tp1 <= y <= tpmin) by crush; generalize hyp noRecvGTTp1; clear; firstorder).
         assert (tc2LtTcmin: S tc2 <= tcmin) by crush.
